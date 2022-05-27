d0522c24ef1370420569d59ad6d1394262260835
angeldavid2002 <70535423+angeldavid2002@users.noreply.github.com>
2022-05-26 22:34:33 -0500

    avance lista usuarios

--- lib/vistas/vista_usuario.dart
+++ lib/vistas/vista_usuario.dart
@@ -5,8 +5,39 @@ class VistaUsuario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(home: PaginaInicio());
+  }
+}
+
+class PaginaInicio extends StatelessWidget {
+  const PaginaInicio({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("pagina inicio"),
+      ),
+      body: Lista(),
+    );
+  }
+}
+
+class Lista extends StatelessWidget {
+  const Lista({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          child: Image.network("https://image.shutterstock.com/image-vector/young-man-lodger-vector-illustration-260nw-654168382.jpg"),
+        );
+      },
     );
   }
 }
