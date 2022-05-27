9143b1ca4c4ca59260cb7ebc753b65c63f05f767
sscarlethernandez <79415539+sscarlethernandez@users.noreply.github.com>
2022-05-27 02:00:50 -0500

    VistaRolOK

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
       home: VistaRol(),
     );
   }
--- lib/vistas/seleccion_rol.dart
+++ lib/vistas/seleccion_rol.dart
@@ -25,23 +25,74 @@ class _VistaRolState extends State<VistaRol> {
                 backgroundImage: AssetImage('images/logotendero.png'),
               ),
               Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Color.fromRGBO(255, 227, 183, 1),
-                ),
-                width: 400,
-                height: 250,
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  'Seleccione su tipo de rol',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontFamily: 'Helvetica',
-                      fontSize: 20.0,
-                      color: Color.fromRGBO(254, 74, 86, 1)),
-                ),
-              ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromRGBO(255, 227, 183, 1),
+                  ),
+                  width: 400,
+                  height: 250,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Seleccione su tipo de rol',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: 'Helvetica',
+                            fontSize: 25.0,
+                            color: Color.fromRGBO(254, 74, 86, 1)),
+                      ),
+                      SizedBox(height: 35),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromRGBO(244, 48, 85, 1),
+                            fixedSize: const Size(300, 50),
+                          ),
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Tendero',
+                                style: TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 18.0,
+                                    color: Colors.white),
+                              ),
+                              Container(
+                                child: Image.asset("images/tendero.png"),
+                                width: 40,
+                                height: 40,
+                              )
+                            ],
+                          )),
+                      SizedBox(height: 30),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromRGBO(244, 48, 85, 1),
+                            fixedSize: const Size(300, 50),
+                          ),
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Cliente',
+                                style: TextStyle(
+                                    fontFamily: 'Helvetica',
+                                    fontSize: 18.0,
+                                    color: Colors.white),
+                              ),
+                              Container(
+                                child: Image.asset("images/cliente.png"),
+                                width: 40,
+                                height: 40,
+                              )
+                            ],
+                          )),
+                    ],
+                  )),
             ],
           )
         ],
