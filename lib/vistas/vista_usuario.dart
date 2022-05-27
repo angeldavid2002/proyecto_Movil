7e28dc8546752e79d19ac9b04cf8f9a70e560cb8
angeldavid2002 <70535423+angeldavid2002@users.noreply.github.com>
2022-05-26 22:03:07 -0500

    widget vista usuario

--- lib/vistas/vista_usuario.dart
+++ lib/vistas/vista_usuario.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
-
 class VistaUsuario extends StatelessWidget {
   const VistaUsuario({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    );
+    return Scaffold();
   }
 }
-
